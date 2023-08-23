0e4909110570a16b4d7547d3333b5f1544b62b19
Localcoder <wiut00011625@gmail.com>
2023-08-23 14:16:59 +0500

    find Ancestor State of Type

---
+++ lib/examples.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Examples extends StatefulWidget {
+  const Examples({super.key});
+
+  @override
+  State<Examples> createState() => _ExamplesState();
+}
+
+class _ExamplesState extends State<Examples> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
